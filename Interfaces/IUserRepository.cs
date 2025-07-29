a484ce9f9a0ae25ace0ae1fc5255d47bc1f72c41
Shashank Mishra <shashankmis808@gmail.com>
2025-07-29 21:11:54 +0530

    Add user management methods to IUserRepository

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -5,7 +5,10 @@ namespace UserDirectory.Interfaces
 {
     public interface IUserRepository
     {
+        void AddUser(User user);
+        bool DeleteUser(int id);
         List<User> GetAllUsers();
         User? GetUserById(int id);
+        bool UpdateUser(int id, User user);
     }
 }
--- Repository/UserService.cs
+++ Repository/UserService.cs
@@ -27,6 +27,40 @@ namespace UserDirectory.Repository
 
         public User? GetUserById(int id) => _users.FirstOrDefault(u => u.Id == id);
 
+        public void AddUser(User user)
+        {
+            user.Id = _users.Any() ? _users.Max(u => u.Id) + 1 : 1;
+            _users.Add(user);
+            SaveChanges();
+        }
+
+        public bool UpdateUser(int id, User user)
+        {
+            var existingUser = GetUserById(id);
+            if (existingUser == null) return false;
+
+            existingUser.Name = user.Name;
+            existingUser.Username = user.Username;
+            existingUser.Email = user.Email;
+            existingUser.Address = user.Address;
+            existingUser.Phone = user.Phone;
+            existingUser.Website = user.Website;
+            existingUser.Company = user.Company;
+
+            SaveChanges();
+            return true;
+        }
+
+        public bool DeleteUser(int id)
+        {
+            var user = GetUserById(id);
+            if (user == null) return false;
+
+            _users.Remove(user);
+            SaveChanges();
+            return true;
+        }
+
         private void SaveChanges()
         {
             var json = JsonSerializer.Serialize(_users, new JsonSerializerOptions { WriteIndented = true });
